1d0d97d17a4707b569aa894b4ad0cc5a0fca0d82
lunz1207 <lunz1207@gmail.com>
2022-06-19 20:06:17 +0800

    chore: route

--- src/main.py
+++ src/main.py
@@ -5,11 +5,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/demo")
 async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/demo/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
