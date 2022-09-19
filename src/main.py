3cc7af79c9effc333d5e6246112e84d60793a5d5
lunz1207 <lunz1207@gmail.com>
2022-09-19 14:29:32 +0800

    fix: path

--- src/main.py
+++ src/main.py
@@ -15,6 +15,6 @@ async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("/items/")
+@app.get("/demo/items/")
 async def read_items(skip: int = 0, limit: int = 10):
     return {'skip':skip,'limit':limit}
