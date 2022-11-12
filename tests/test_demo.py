656773c258ffbcd838f9bfa40cd1aeb6e5f7c286
lunz1207 <lunz1207@gmail.com>
2022-11-12 21:16:40 +0800

    fix: cases and deploy

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -14,7 +14,7 @@ class TestDemo():
     }
 
     def test_read_root(self):
-        resp = self.bs.get('/demo/')
+        resp = self.bs.get('/demo/', headers=self.header)
         assert resp.status_code == 200
 
     def test_read_item(self):
@@ -22,5 +22,6 @@ class TestDemo():
         assert resp.status_code == 200
 
     def test_anythings(self):
-        resp = self.bs.post('/demo/items', json=self.payload)
+        resp = self.bs.post(
+            '/demo/items', json=self.payload, headers=self.header)
         assert resp.status_code == 200
