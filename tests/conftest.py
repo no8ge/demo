f6a62622a74d4524ecdc6cbfabe00c4f1453ca79
lunzhou <lunzhou@yunify.com>
2023-08-18 14:07:56 +0800

    chore: tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,19 @@
 import os
 import pytest
-
-from requests_toolbelt.sessions import BaseUrlSession
+from requests import Session
 
 
 env = os.getenv('ENV')
-envs = {
-    'loc': 'http://127.0.0.1:8002',
-    'dev': 'http://tink.dev:31694',
-    'test': 'http://tink.test:31695',
-    'prduction': 'http://tink.com:31696',
-}
+host = os.getenv('HOST')
 
 
 @pytest.fixture()
 def init(request):
-    url = envs[env]
-    bs = BaseUrlSession(base_url=url)
+    bs = Session()
+    bs.headers['Authorization'] = 'admin'
     request.cls.bs = bs
+    request.cls.env = env
+    if env == 'loc':
+        request.cls.url = f'http://127.0.0.1:8002'
+    else:
+        request.cls.url = f'http://test-demo.atop:8002'
--- tests/test_demo.py
+++ tests/test_demo.py
@@ -14,14 +14,14 @@ class TestDemo():
     }
 
     def test_read_root(self):
-        resp = self.bs.get('/demo/', headers=self.header)
+        resp = self.bs.get(f'{self.url}/demo/', headers=self.header)
         assert resp.status_code == 200
 
     def test_read_item(self):
-        resp = self.bs.get('/demo/1', headers=self.header)
+        resp = self.bs.get(f'{self.url}/demo/404', headers=self.header)
         assert resp.status_code == 200
 
     def test_anythings(self):
         resp = self.bs.post(
-            '/demo/items', json=self.payload, headers=self.header)
+            f'{self.url}/demo/items', json=self.payload, headers=self.header)
         assert resp.status_code == 200
